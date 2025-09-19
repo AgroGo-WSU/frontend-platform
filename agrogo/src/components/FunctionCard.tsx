6140e47ef174e4d907a7284b2a73d1183ee7e27d
Madeline Bartley <madeline.bartley@outlook.com>
2025-09-18 20:34:26 -0400

    Set up first component and stylesheet

---
+++ agrogo/src/components/FunctionCard.tsx
@@ -0,0 +1,16 @@
+import "../stylesheets/FunctionCard.css"
+
+
+function FunctionCard() {
+
+    return (
+        <div className="function-card">
+            im a div container
+            <div className="go">
+                I'm a div
+            </div>
+        </div>
+    )
+}
+
+export default FunctionCard
