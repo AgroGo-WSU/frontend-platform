fab0e2dc7e2b0fd06ad44f0d0ffad37e5e66f8ca
Madeline Bartley <madeline.bartley@outlook.com>
2025-09-19 00:15:57 -0400

    Added the css grid to the main page to match our mockup

--- agrogo/src/App.tsx
+++ agrogo/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import './stylesheets/App.css'
 import FunctionCard from './components/FunctionCard'
 
 function App() {
 
   return (
-    <div>
-      <FunctionCard/>
+    <div className="dashboard">
+      <div className="top-blank">I'm top blank</div>
+      <div className="bottom-blank">I'm bottom blank</div>
+      <div className="left-menu">I'm left menu</div>
+      <div className="right-blank">I'm right blank</div>
+      <div className="topnav">I'm the topnav</div>
+      <div className="welcome-title">I'm the welcome title</div>
+      <div className="weather">I'm the weather cards</div>
+      <div className="zones">I'm the zones<FunctionCard/></div>
+      <div className="footer">I'm the footer</div>
     </div>
   )
 }
