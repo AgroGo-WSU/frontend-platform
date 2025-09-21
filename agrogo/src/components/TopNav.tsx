6b076a7e986ee47c2020db45501e54a62e0840fb
Madeline Bartley <madeline.bartley@outlook.com>
2025-09-21 16:55:35 -0400

    Finished adding props to components and other minor tweaks to the styleguide

--- agrogo/src/components/TopNav.tsx
+++ agrogo/src/components/TopNav.tsx
@@ -18,22 +18,22 @@ function TopNav() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Weather</Nav.Link>
-            <Nav.Link href="#pricing">Inventory</Nav.Link>
+            <Nav.Link href="#weather">Weather</Nav.Link>
+            <Nav.Link href="#inventory">Inventory</Nav.Link>
             <NavDropdown title="Actions" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Set watering</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Set fan</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">View greenhouse stats</NavDropdown.Item>
+              <NavDropdown.Item href="#watering">Set watering</NavDropdown.Item>
+              <NavDropdown.Item href="#fan">Set fan</NavDropdown.Item>
+              <NavDropdown.Item href="#greenhouse-stats">View greenhouse stats</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item href="#manage-zones">
                 Manage zones
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="#notifications"><img className="icon-img" src="../src/assets/notifications-icon-173D23.svg" width="10px"></img></Nav.Link>
-            <Nav.Link eventKey={2} href="#profile"><img className="icon-img" src="../src/assets/profile-icon-173D23.svg"></img></Nav.Link>
-            <Nav.Link eventKey={2} href="#settings"><img className="icon-img" src="../src/assets/settings-icon-173D23.svg"></img></Nav.Link>
+            <Nav.Link href="#notifications"><img className="icon-img" src="../src/assets/icons/notifications-icon-173D23.svg" width="10px"></img></Nav.Link>
+            <Nav.Link eventKey={2} href="#profile"><img className="icon-img" src="../src/assets/icons/profile-icon-173D23.svg"></img></Nav.Link>
+            <Nav.Link eventKey={2} href="#settings"><img className="icon-img" src="../src/assets/icons/settings-icon-173D23.svg"></img></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- agrogo/src/components/WeatherCard.tsx
+++ agrogo/src/components/WeatherCard.tsx
@@ -1,14 +1,26 @@
 import '../stylesheets/WeatherCard.css';
 
 
-function WeatherCard() {
+interface WeatherCardProps {
+  /* These are the props that must be passed when this card is rendered on the parent component - go to App.tsx to see how these props are being passed to this component */
+  /* Later, these will be passed programmatically via mapping or other function, but for now, they are hard-coded in the parent component */
+  /* The day of the week, the date, the weather, and the image file */
+  /* The string for image file must be in this form: "../src/assets/weather-images/sun.png" where "weather" can be replaced by "sun", "partial-clouds", or "rain"*/
+  day: string;
+  date: string;
+  weather: string;
+  image: string;
+}
+
+
+function WeatherCard({ day, date, weather, image }: WeatherCardProps) {
   return (
     <div className="weather-card-container">
+      <img className="weather-card-image" src={ image }></img>
       <div className="card-flex-container">
-        <img className="weather-card-image" src="../src/assets/sun.png"></img>
-        <div className="day-caption">Today</div>
-        <div className="weather-card-title">Sept. 21</div>
-        <div className="weather-card-subtitle">Looks like it's gonna be sunny or smth</div>
+        <div className="day-caption">{ day }</div>
+        <div className="weather-card-title">{ date }</div>
+        <div className="weather-card-subtitle">{ weather }</div>
       </div>
     </div>
   );
