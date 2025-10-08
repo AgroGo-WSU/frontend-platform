20e10daaacadec2734a0eb4f6016def444e4cf29
Madeline Bartley <madeline.bartley@outlook.com>
2025-10-07 21:53:19 -0400

    Adding form elements

--- agrogo/src/components/AppContainer/AppContainer.tsx
+++ agrogo/src/components/AppContainer/AppContainer.tsx
@@ -2,6 +2,7 @@ import "../../stylesheets/AppContainer.css";
 import Dashboard from "../Dashboard/Dashboard";
 import AuthenticationPopup from "../Auth/AuthenticationPopup";
 import { useAuth } from "../../hooks/UseAuth.tsx";
+import ProfileCreation from "../Profile/ProfileCreation.tsx";
 
 function AppContainer() {
     const { userLoggedIn } = useAuth();
@@ -9,7 +10,8 @@ function AppContainer() {
     if(userLoggedIn) {
         return(
             <div>
-                <Dashboard />
+                <ProfileCreation />
+                {/* <Dashboard /> */}
             </div>
         )
     } else if(!userLoggedIn) {
--- agrogo/src/components/Profile/ProfileCreation.tsx
+++ agrogo/src/components/Profile/ProfileCreation.tsx
@@ -1,8 +1,23 @@
 import "../../stylesheets/ProfileCreation.css"
+import SmallTitle from "../SmallTitle";
 
 function ProfileCreation() {
 
-    return(<div></div>)
+    return(
+        <div className="profile-creation-container">
+            <SmallTitle title="Create your profile"/>
+            <div className="profile-form-container">
+             <form>
+                <label htmlFor="first-name">First name:</label>
+                <input type="text" id="first-name" name="first-name"></input>
+                <label htmlFor="last-name">Last name:</label>
+                <input type="text" id="last-name" name="last-name"></input>
+                <label htmlFor="location">Location:</label>
+                <input type="text" id="location" name="location"></input>
+            </form>
+            </div> 
+        </div>
+        )
 }
 
 export default ProfileCreation;
