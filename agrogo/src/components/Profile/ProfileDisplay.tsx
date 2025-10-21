380fe93d5e291a2bc1d23502e8b2f2b8100f08f6
Madeline Bartley <madeline.bartley@outlook.com>
2025-10-20 21:13:28 -0400

    Added dynamic usernames to the profile and dashboard

--- agrogo/src/components/Profile/ProfileDisplay.tsx
+++ agrogo/src/components/Profile/ProfileDisplay.tsx
@@ -1,16 +1,26 @@
 import "../../stylesheets/ProfileDisplay.css";
 import ProfileImage from "./ProfileImage";
 import PlanBubble from "../Plan/PlanBubble";
+import { useContext } from 'react';
+import { AuthContext } from '../../hooks/UseAuth';
 
 function ProfileDisplay() {
+
+    // grabbing our current user from the Authentication context we created
+    const { currentUser } = useContext(AuthContext);
+  
+    let userName = "friend";
+    if(currentUser) {
+      userName = currentUser.displayName;
+    }
+
   return (
     <div>
       {/* Profile Card */}
       <div className="profile-display-container">
         <ProfileImage />
         {/* Example placeholders of the info that will go here */}
-        <div className="name">Madeline</div>
-        <div className="location">Detroit, MI</div>
+        <div className="name">{userName}</div>
         <div className="next-water">Next water: Monday, 5pm</div>
         <div className="next-fan">Next fan: Monday, 5:15pm</div>
         <div className="growing-since">Growing since 2025</div>
