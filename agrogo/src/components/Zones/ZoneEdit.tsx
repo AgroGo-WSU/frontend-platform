5adc51ae2549460f3c1cfbaa6028e63a3f52b575
Madeline Bartley <madeline.bartley@outlook.com>
2025-11-07 16:38:18 -0500

    Hit error - pushing everything

--- agrogo/src/components/Zones/ZoneEdit.tsx
+++ agrogo/src/components/Zones/ZoneEdit.tsx
@@ -1,3 +1,4 @@
+import '../../stylesheets/ZoneEdit.css';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import axios from "axios";
@@ -27,9 +28,12 @@ function ZoneEdit(props) {
   const [data, setData] = useState(null);
   const [sendingWaterSchedule, setSendingWaterSchedule] = useState(false);
   const [updatedWaterInput, setUpdatedWaterInput] = useState(null);
+  const [newEntry, setNewEntry] = useState(false);
   const [waterSchedZone1, setWaterSchedZone1] = useState();
   const [waterSchedZone2, setWaterSchedZone2] = useState();
   const [waterSchedZone3, setWaterSchedZone3] = useState();
+  const [numberOfZones, setNumberOfZones] = useState(0);
+  const [zoneData, setZoneData] = useState([]);
 
   console.log("Example of the props thing - this is the id: ", props.data[0].id);
 
@@ -67,6 +71,162 @@ useEffect(() => {
 
     },[sendingWaterSchedule]);
 
+
+
+    // also clears the previous state
+    const saveChanges = async(event) => {
+
+      console.log("Water schedule event target firstchild ", event.target.firstChild.data);
+      const eventTYPE = event.target.firstChild.data;
+      // const eventID = event.target.id;
+      const bothIDs = event.target.id.split("_");
+      const zoneArrayID = bothIDs[0];
+      const zoneArrayIndex = Number(bothIDs[1]);
+
+      let findData = [];
+
+      console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", waterSchedZone1);
+
+      // find which array of data to take form:
+      if(zoneArrayID === "1") {
+        findData = waterSchedZone1;
+      } else if(zoneArrayID === "2") {
+        findData = waterSchedZone2;
+      } else if(zoneArrayID === "3") {
+        findData = waterSchedZone3;
+      }
+
+      setNewEntry(false);
+      // setEditing(false);
+
+      console.log("******************************************************************", zoneArrayIndex, findData[zoneArrayIndex]);
+
+      const persistentItemID = findData[zoneArrayIndex].id;
+
+
+      // try to get the user token and make the post request
+        try {
+        const auth = getAuth();
+        const user = auth.currentUser;
+
+
+        if (!user) {
+          throw new Error('User not authenticated!');
+        }
+
+        // const userIdFB = user.uid;
+        const token = await user.getIdToken();
+      
+        if (eventTYPE === "Save entry") {
+
+        const postData = {
+          id: findData[zoneArrayIndex].id,
+          type: findData[zoneArrayIndex].type,
+          userId: findData[zoneArrayIndex].userId,
+          scheduledTime: findData[zoneArrayIndex].scheduledTime,
+          duration: findData[zoneArrayIndex].duration,
+        }
+
+        const sentResponse = await axios.post("https://backend.agrogodev.workers.dev/api/data/waterSchedule", postData, {
+              headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+                }
+            });
+          
+          setSendingWaterSchedule(true);
+          console.log("Sending POST request for watering schedule new data - here's the response ", sentResponse);
+          } else if(eventTYPE === "Update entry") {
+
+        
+        const putData = {
+          id: findData[zoneArrayIndex].id,
+          type: findData[zoneArrayIndex].type,
+          userId: findData[zoneArrayIndex].userId,
+          scheduledTime: findData[zoneArrayIndex].scheduledTime,
+          duration: findData[zoneArrayIndex].duration,
+        }
+        
+        const sentResponse = await axios.put("https://backend.agrogodev.workers.dev/api/data/waterSchedule", putData, {
+              headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+                }
+            });
+          
+          setSendingWaterSchedule(true);
+          console.log("Sending PUT request for watering schedule new data - here's the response ", sentResponse);           
+          } else if(eventTYPE === "Delete entry") {
+
+            console.log("!!! MAKING DELETE REQUEST: ", findData[zoneArrayIndex].id);
+        const sentResponse = await axios.delete("https://backend.agrogodev.workers.dev/api/data/waterSchedule", {
+              headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+                },
+              data: {
+                id: findData[zoneArrayIndex].id
+              }
+            });
+          
+          setSendingWaterSchedule(true);
+          console.log("Sending DELETE request for new data - here's the response ", sentResponse);           
+          } 
+
+          } catch(error){
+            console.error('Error sending data:', error);
+          } finally {
+            console.log("Looks like the water schedule DELETE request worked!");
+      }
+
+      setSendingWaterSchedule(true);
+      setEntryBeingEdited("0");
+    }
+
+    // for users to add or remove zones
+    useEffect(() => {
+      const getZoneData = async () => {
+
+        try {
+        const auth = getAuth();
+        const user = auth.currentUser;
+
+        if (!user) {
+          throw new Error('User not authenticated!');
+        }
+
+        const token = await user.getIdToken();
+      
+        const response = await axios.get("https://backend.agrogodev.workers.dev/api/user/zone", {
+              headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+                }
+            });
+
+          console.log("Setting zone data: ", response.data.data);
+          setZoneData(response.data.data);
+          } catch(error){
+            console.error('Error fetching zone data:', error);
+          } finally {
+            console.log("Final data for zone: ", zoneData);
+      }
+    };
+
+    getZoneData();
+
+    },[numberOfZones]);
+
+  function addZone() {
+    setNumberOfZones(numberOfZones+1);
+    console.log(numberOfZones);
+  }
+
+  function removeZone() {
+    setNumberOfZones(numberOfZones-1);
+    console.log(numberOfZones);
+  }
+
   function updateWateringTime(event) {
     console.log("Updating a watering time for zone ", event.target.id);
     setEntryBeingEdited(event.target.id);
@@ -77,28 +237,40 @@ useEffect(() => {
     console.log("Changing water time!");
   }
 
-  // in the response, you will get everything in the database - you need x number of arrays of times, one fore each zone 
-  const zone1Data = [];
-  const zone2Data = [];
-  const zone3Data = [];
-  var whichZones = [];
 
-  if(data != null) {
-    for(let i = 0; i < data.length; i++ ) {
-      if(data[i].type === "1") {
-        zone1Data.push(data[i]);
-        whichZones = [1];
-      } else if(data[i].type === "2") {
-        zone2Data.push(data[i]);
-        whichZones = [1, 2];
-      } else if (data[i].type ==="3") {
-        zone3Data.push(data[i]);
-        whichZones = [1, 2, 3];
+    // in the response, you will get everything in the database - you need x number of arrays of times, one fore each zone 
+    const zone1Data = [];
+    const zone2Data = [];
+    const zone3Data = [];
+    var whichZones = [];
+
+    if(data != null) {
+      for(let i = 0; i < data.length; i++ ) {
+        if(data[i].type === "1") {
+          zone1Data.push(data[i]);
+          whichZones = [1];
+        } else if(data[i].type === "2") {
+          zone2Data.push(data[i]);
+          whichZones = [1, 2];
+        } else if (data[i].type ==="3") {
+          zone3Data.push(data[i]);
+          whichZones = [1, 2, 3];
+        }
       }
     }
-  }
 
-  console.log("UNDEFINED", zone1Data[1]);
+      // set zone data into state so it can be accessed above
+    useEffect(() => {
+    const setZoneArrays = async() => {
+      setWaterSchedZone1(zone1Data);
+      setWaterSchedZone2(zone2Data);
+      setWaterSchedZone3(zone3Data);
+    }
+      setZoneArrays();
+    }, [entryBeingEdited])
+
+
+
 
   return (
     <Modal
@@ -116,18 +288,26 @@ useEffect(() => {
         {/** Have the user set times and make the post request run every time they click send
          * This means, make the get request from the water schedule, set the waterSchedule data, if it's there, map it
          */}
-
+        <div className="add-zone-button">{numberOfZones === 0 ? <button onClick={addZone} className="add-a-zone">Add a zone</button> : numberOfZones >= 3 ? <div><button onClick={addZone} className="max-zones-reached">Max zones reached</button><button onClick={removeZone} className="remove-zone">Remove a zone</button></div> : <button onClick={addZone} className="add-a-zone">Max zones</button>}</div>
         <div>{zone1Data.length > 0 ?
             <div className="zones">
                 <div id="1">Zone 1</div>
                 <div id="1">
                   <div className="watering-data">
                     {zone1Data.map((item, index: number) => (  
-                        entryBeingEdited != "1_"+index ? <div className="display-water-data">
-                        {item.time}<button id={"1_" + index} onClick={updateWateringTime}>Update watering time</button></div>
+                      entryBeingEdited != "1_"+index ? 
+                      <div className="display-water-data">
+                        {item.time}<button id={"1_" + index} onClick={updateWateringTime}>Update watering time</button>
+                      </div>
                        : 
-                      <div className="current-water-schedule"><div id={"1_" + index}><input type="text" name="edit_change" id={"1_" + index} value={updatedWaterInput} onChange={handleChangeWaterTime}></input></div></div>
-                        ))}
+                      <div className="current-water-schedule">
+                        <div id={"1_" + index}>
+                          <input type="text" name="edit_change" id={"1_" + index} value={updatedWaterInput} onChange={handleChangeWaterTime}></input>
+                          <button id={"1_" + index} className="save-changes" onClick={saveChanges}>Update entry</button>
+                          <button id={"1_" + index} className="delete-changes" onClick={saveChanges}>Delete entry</button>
+                        </div>
+                      </div>
+                    ))}
                   </div> 
                 </div>
             </div> : <></>}
@@ -142,7 +322,7 @@ useEffect(() => {
                         entryBeingEdited != "2_"+index ? <div className="display-water-data">
                         {item.time}<button id={"2_" + index} onClick={updateWateringTime}>Update watering time</button></div>
                        : 
-                      <div className="current-water-schedule"><div id={"2_" + index}><input type="text" name="edit_change" id={"2_" + index} value={updatedWaterInput} onChange={handleChangeWaterTime}></input></div></div>
+                      <div className="current-water-schedule"><div id={"2_" + index}><input type="text" name="edit_change" id={"2_" + index} value={updatedWaterInput} onChange={handleChangeWaterTime}></input><button id={"2_" + index} className="save-changes" onClick={saveChanges}>Update entry</button></div></div>
                         ))}
                   </div> 
                 </div>
@@ -158,33 +338,12 @@ useEffect(() => {
                         entryBeingEdited != "3_"+index ? <div className="display-water-data">
                         {item.time}<button id={"3_" + index} onClick={updateWateringTime}>Update watering time</button></div>
                        : 
-                      <div className="current-water-schedule"><div id={"3_" + index}><input type="text" name="edit_change" id={"3_" + index} value={updatedWaterInput} onChange={handleChangeWaterTime}></input></div></div>
+                      <div className="current-water-schedule"><div id={"3_" + index}><input type="text" name="edit_change" id={"3_" + index} value={updatedWaterInput} onChange={handleChangeWaterTime}></input><button id={"3_" + index} className="save-changes" onClick={saveChanges}>Update entry</button></div></div>
                         ))}
                   </div> 
                 </div>
             </div> : <></>}
         </div>
-
-        
-
-
-          {/* <div className="zone">
-            {editing === false ? typeData.map(item => (
-              <div className="current-water-schedule"><div id={item[1]}>
-              value={item[0]}</div></div>)) :
-              
-              typeData.map(item => ( item[1].toString() === columnBeingEdited ? 
-              <div className="current-water-schedule"><div id={item[1]}><input type="text" name="edit_change" value={waterStartTime} onChange={handleChangeType}></input></input></div></div> : 
-              <div className="current-water-schedule"><div id={item[1]}><InventoryPlantItem value={item[0]} /></div></div>
-            ))}
-
-            {newEntry === true ? (
-              <div className="table-data">
-                <label htmlFor="input_name">Plant type</label><br />
-                <input type="text" name="input_name" onChange={handleChangeType}></input>
-              </div>
-            ) : <></>}
-          </div> */}
 
       </Modal.Body>
       <Modal.Footer>
