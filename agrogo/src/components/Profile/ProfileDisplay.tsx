c6b882ef73b06c6be8c76d9599eae57765fcd39e
andrew-adomaitis <theelderone02@gmail.com>
2025-11-08 20:07:34 -0500

    First step of making the profile page functional

--- agrogo/src/components/Profile/ProfileDisplay.tsx
+++ agrogo/src/components/Profile/ProfileDisplay.tsx
@@ -1,26 +1,60 @@
 import "../../stylesheets/ProfileDisplay.css";
 import "../../stylesheets/SidebarColumn.css"; // NEW
 import ProfileImage from "./ProfileImage";
-import PlanBubble from "../Plan/PlanBubble";
-import NotificationsPanel from "../Notification/NotificationsPanel";
-import { useNotifications } from "../../hooks/UseNotifications";
 import ProfileMini from "./ProfileMini";
-import Humidity from '../../components/Humidity';
-import Temp from '../../components/Temp';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../hooks/UseAuth';
-import ConnectivityStatus from "../ConnectivityStatus";
+import axios from "axios";
+import { getAuth } from "firebase/auth";
 
 function ProfileDisplay() {
-    const { items, clear } = useNotifications();
+  // grabbing our current user from the Authentication context we created
+  const { currentUser } = useContext(AuthContext);
 
-    // grabbing our current user from the Authentication context we created
-    const { currentUser } = useContext(AuthContext);
-  
-    let userName = "friend";
-    if(currentUser) {
-      userName = currentUser.displayName!;
+  // Little confusing, but this is the user's data in D1, the currentUser
+  // field is the data from Firebase
+  const [user, setUser] = useState({});
+
+  let userName = "friend";
+  if(currentUser) {
+    userName = currentUser.displayName!;
+  }
+
+  async function getUserDetails() {
+    try {
+      const auth = getAuth();
+      const user = auth.currentUser;
+
+      if(!user) {
+        throw new Error("No authenticated user found");
+      }
+
+      const token = await user.getIdToken();
+
+      // Call the backend API route that finds the user's info from D1
+      const userRes = await axios.get(
+        "https://backend.agrogodev.workers.dev/api/user/user",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      console.log("user data:", userRes.data.data[0]);
+      return userRes.data.data[0];
+    } catch(err) {
+      console.log("Error fetching user data from the backend:", err);
     }
+  }
+
+  useEffect(() => {
+    async function fetchUser() {
+      const data = await setUser(getUserDetails());
+      if(data) setUser(data);
+    }
+    fetchUser();
+  }, [])
 
   return (
     <div className="profile-display-all">
@@ -28,29 +62,17 @@ function ProfileDisplay() {
       {/* Profile Card */}
       <div className="profile-display-container d-none d-xl-block">
         <ProfileImage />
+        <button onClick={getUserDetails}>Get user details</button>
 
-      <div className="name">{userName}</div>
-
-      {/* Today’s Plan */}
-      <PlanBubble />
-
-      {/* connection, humidity, temp */}
-      <div className="d-none d-xl-block"><ConnectivityStatus /></div>
-      <div className="d-none d-xl-block"><Humidity /></div>
-      <div className="d-none d-xl-block"><Temp /></div>
-
-      {/* Today’s Plan bubble
-      <div className="d-none d-xl-block">
-      <PlanBubble />
-      </div> */}
+      <div className="name">{user.firstName} {user.lastName}</div>
+      <div className="start-date">Member since {user.createdAt}</div>
 
       <div className="profile-mini d-xl-none">
 			  <ProfileMini />
 		  </div>
         
-           {/* Notifications */}
-      <NotificationsPanel items={items} onClearAll={clear} />
-        {/* Example placeholders of the info that will go here */}
+        {/* Notifications */}
+        {/* <NotificationsPanel items={items} onClearAll={clear} /> */}
     
       </div>
       </aside>
