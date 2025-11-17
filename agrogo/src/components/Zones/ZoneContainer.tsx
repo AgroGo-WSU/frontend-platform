ea06f3094c0b57aba3b2499ef833c403d71d31ca
Madeline Bartley <madeline.bartley@outlook.com>
2025-11-17 18:32:09 -0500

    Cleaning up zone edit page and fixing bug on dashboard

--- agrogo/src/components/Zones/ZoneContainer.tsx
+++ agrogo/src/components/Zones/ZoneContainer.tsx
@@ -60,9 +60,6 @@ function ZoneContainer() {
         <div className="zone-container-top">
         {modalShow === true ? <div className="edit-modal"><ZoneEdit show={modalShow} onHide={() => setModalShow(false)} data={zoneData}/></div> : <></>}
         <div className="zone-flex-container d-flex flex-column flex-xl-row justify-content-between">
-          {/* <ZoneCard plants="Carrots and cucumbers"        image="../src/assets/zone-images/vegetable.png"/>
-          <ZoneCard plants="Cosmos and petunias"          image="../src/assets/zone-images/flower.png"/>
-          <ZoneCard plants="Peppers, lavender, kale"      image="../src/assets/zone-images/plant.png"/> */}
           {zoneData != null ? zoneData.map(item =>(
             <ZoneCard plants={item.description} image={item.zoneType} />
           )) : <></>
